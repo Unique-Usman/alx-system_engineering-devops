d3277ff7c1718a40885b7b645a430b6497b64947
Unique-Usman <usmanakinyemi202@gmail.com>
2024-03-28 15:19:08 +0530

    Completed the API Advanced taks

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+"""
+A function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a gives
+subreddit. If an invalid subreddit is given, the function should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    The function that returns the number of
+    suscribers.
+
+    Args:
+        subreddit (str): The subreddit
+    """
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'UsmanBot/1.0'})
+    r = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                     headers=headers, allow_redirects=False)
+    sub = r.json().get("data", {}).get("subscribers")
+
+    if sub:
+        return sub
+    return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+"""
+A function that returns top ten
+hot topic for a particular subredit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Returns the number of top ten hot posts
+    for a particular subreddit
+
+    Args:
+        subreddit (str): The subreddit
+    """
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'UsmanBot/1.0'})
+    r = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json",
+                     headers=headers, allow_redirects=False,
+                     params={"limits": 9})
+    res = r.json()
+    children = res.get('data', {}).get('children')
+    if not children:
+        print(None)
+    else:
+        for child in children:
+            print(child.get('data', None).get('title', None))
